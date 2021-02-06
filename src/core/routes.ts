ab53a28f2693a09b67783cb4b802a4f5a02c6f44
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-02-06 16:45:57 +0300

    Adding forum link

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -4,3 +4,5 @@ export const pricing = `/pricing`,
 const content = '/content';
 export const about = `${content}/about`,
   terms = `${content}/terms`;
+
+export const forum = 'https://forum.safeapps.io/';
